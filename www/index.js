bfd65c0e081e0c8cd2603096e3a87a3e80bbd071
Huong <huongnguyenak96@gmail.com>
2019-03-27 19:12:39 +0700

    Calculate

--- www/index.js
+++ www/index.js
@@ -8,14 +8,43 @@ app.get('/',(request, response)=>{
 })
 app.get('/:name/:age',(req,res)=>{
     // let myname = req.params.name
-    // let age = req.params.age
+    // let age = +req.params.age
 
-    // let { name, age } = req.params
-    // res.send({name,age})
+    let { name, age} = req.params
+    res.send({name,age})
 })
+app.get('/:pheptinh/:a/:b',(req,res)=>{
+    const { pheptinh, a, b } = req.params
+    if(pheptinh=='chia' && b == 0)
+    res.send({
+        error: 'Error!'
+    })
+    const cal = new Calculate(pheptinh, a, b)
+    res.send({
+        result: cal.getResult()
+    })  
+})
+
 class Calculate{
-    
+    constructor(pt, a, b){
+        this.pt = pt,
+        this.a = a;
+        this.b = b;
+    }
+    get pheptinh(){
+        if(this.pt=='cong') return '+'
+        if(this.pt=='tru') return '-'
+        if(this.pt=='nhan') return '*'
+        return '/'
+    }
+    getResult(){
+        let r = this.a + this.pheptinh + this.b // 3+4
+        // return r+'='+eval(r);
+        return `${r} = ${eval(r)}`
+    }
 }
+
+
 
 
 app.listen(3000,()=>{
